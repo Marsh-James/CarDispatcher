db32637ae4f214aa28a33c82671c6cba58a716c0
James Marsh <jamesfkmarsh@gmail.com>
2018-03-01 19:02:06 +0000

    added cars and human methods

--- entities/Car.py
+++ entities/Car.py
@@ -2,4 +2,13 @@ class car:
     def __init__(self):
         self.pos_x = 0
         self.pos_y = 0
-        self.timeout = 0
+        self.turn_available = 0
+        self.in_use = False
+
+    # calculate turn available
+    # set when car is in use
+    def set_available(self):
+        self.in_use = True
+
+    def set_unavailable(self):
+        self.in_use = False
--- entities/Human.py
+++ entities/Human.py
@@ -1,7 +1,19 @@
 class human:
-    def __init__(self):
+    def __init__(self, bonus_points):
         self.pos_x = 0
         self.pos_y = 0
         self.earliest_start = 0
         self.latest_finish = 0
         self.distance = 0
+        self.points_obtained = 0
+        self.bonus_points = bonus_points
+
+    # math for earliest pickup points gain
+    def get_distance(self, final_x, final_y):
+        self.distance = abs(final_x - self.pos_x) + abs(final_y - self.pos_y)
+
+    def ride_start(self):
+        
+
+    def ride_finished(self):
+        self.points_obtained = self.distance + self.bonus_points
