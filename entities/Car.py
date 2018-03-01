9323e32a2c659a72bfb3fe5dc49e3a4751138a32
James Marsh <jamesfkmarsh@gmail.com>
2018-03-01 18:44:01 +0000

    added entities classes

---
+++ entities/Car.py
@@ -0,0 +1,5 @@
+class car:
+    def __init__(self):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.timeout = 0
---
+++ entities/Human.py
@@ -0,0 +1,7 @@
+class human:
+    def __init__(self):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.earliest_start = 0
+        self.latest_finish = 0
+        self.distance = 0
