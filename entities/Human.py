08f574da5de23e7c62f799345b81cde0fdc5f039
James Marsh <jamesfkmarsh@gmail.com>
2018-03-01 19:04:25 +0000

    updated init for human

--- entities/Human.py
+++ entities/Human.py
@@ -1,19 +1,19 @@
 class human:
-    def __init__(self, bonus_points):
+    def __init__(self, on_time):
         self.pos_x = 0
         self.pos_y = 0
         self.earliest_start = 0
         self.latest_finish = 0
         self.distance = 0
         self.points_obtained = 0
-        self.bonus_points = bonus_points
+        self.on_time = on_time
 
     # math for earliest pickup points gain
     def get_distance(self, final_x, final_y):
         self.distance = abs(final_x - self.pos_x) + abs(final_y - self.pos_y)
 
     def ride_start(self):
-        
+
 
     def ride_finished(self):
-        self.points_obtained = self.distance + self.bonus_points
+        return self.points_obtained
